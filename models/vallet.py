54f638bfa061bb06228917887c5c6aa26a194641
dadrai <90080470+dadrai@users.noreply.github.com>
2023-10-12 20:02:13 +0700

    Add files via upload

---
+++ models/vallet.py
@@ -0,0 +1,21 @@
+
+#from pydantic import BaseModel
+from config.database import Base
+
+
+
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Float
+from sqlalchemy.orm import relationship
+
+
+
+
+class Vallet(Base):
+    __tablename__ = "vallets"
+
+    id = Column(Integer, primary_key=True, index=True)
+    
+    currency = Column(String)
+    amount = Column(Boolean)
+
+    items = relationship("Item", back_populates="owner")
